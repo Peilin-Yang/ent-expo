a808f2b5f3f4274fd14187c451fba311fa8095ad
Xitong Liu <foremire@gmail.com>
2014-02-14 11:30:36 -0500

    add ent_expo project

---
+++ www/ent_expo/ent_expo/wsgi.py
@@ -0,0 +1,14 @@
+"""
+WSGI config for ent_expo project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
+"""
+
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ent_expo.settings")
+
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
---
+++ www/ent_expo/search/models.py
@@ -0,0 +1,38 @@
+from django.db import models
+
+# Create your models here.
+
+class Query(models.Model) :
+  index = models.IntegerField()
+  title = models.CharField(max_length=100)
+  desc = models.CharField(max_length=256)
+
+  def __unicode__(self) :
+    return '%d - [%s]' %(index, title)
+
+class Document(models.Model) :
+  doc_id = models.CharField(max_length=20)
+  text = models.TextField()
+  stem = models.TextField()
+
+  def __unicode__(self) :
+    return 'Doc - %s' % doc_id
+
+class DocMap(models.Model) :
+  query = models.ForeignKey(Query)
+  doc = models.ForeignKey(Document)
+  is_rel = models.BooleanField()
+
+  def __unicode__(self) :
+    return '%s - %s - %d' %(self.query.index, self.doc.doc_id,
+        self.is_rel)
+
+class DocRank(models.Model) :
+  query = models.ForeignKey(Query)
+  doc = models.ForeignKey(Document)
+  rank = models.IntegerField()
+
+  def __unicode__(self) :
+    return '%s - %d - %s' %(self.query.index, self.rank,
+        self.doc.doc_id)
+
