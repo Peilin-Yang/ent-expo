5a9dcff564dfb45f27b8cba42eec8f076e5a16df
Xitong Liu <foremire@gmail.com>
2014-02-17 17:06:04 -0500

    add templates: search/home.html

--- www/ent_expo/search/views.py
+++ www/ent_expo/search/views.py
@@ -2,10 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.shortcuts import render_to_response, get_object_or_404
 from django.core.urlresolvers import reverse
-
+from search.models import *
 
 def home(request) :
-  return render_to_response('hello.html')
+  query_list = Query.objects.all()
+  return render_to_response('home.html', {'query_list': query_list})
 
 def query(request, query_id) :
   return render_to_response('hello.html')
